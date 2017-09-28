57ceda9e65e84305387eb1022488443480945aa8
Yasser Rachid <anuar.yasser@gmail.com>
2017-09-28 10:01:36 -0300

    Express and Js feature examples

--- pag_inicial.js
+++ pag_inicial.js
@@ -1,32 +1,26 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
-const path = require('path');
+const {join} = require('path')
 
+const lazyLog = (message) => console.log.bind(console, message)
+
+const port = (defaultPort = 3000) => process.env.PORT || defaultPort
+
+const pathFor = (page) => join(`${__dirname}/${page}.html`
+                            
+const render = (buildFileName) => 
+  (request, response) => response.sendFile(pathFor(buildFileName(request)))                          
+                                
 const app = express()
 
-app.set('port', (process.env.PORT || 3000))
+app.get('/', 
+  render(() => 'index'))
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/index.html`));
-})
+app.get('/video/:id', 
+  render(request => `video${request.params.id}`))
 
-app.get('/video1', (req, res) => {
-  res.sendfile(path.join(`${__dirname}/video1.html`))
-})
-app.get('/video2', (req, res) => {
-  res.sendfile(path.join(`${__dirname}/video2.html`))
-})
-app.get('/video3', (req, res) => {
-  res.sendfile(path.join(`${__dirname}/video3.html`))
-})
-app.get('/video4', (req, res) => {
-  res.sendfile(path.join(`${__dirname}/video4.html`))
-})
+app.get('/cool', 
+  (request, response) => response.send(cool())
 
-app.get('/cool', (request, response) => {
-  response.send(cool())
-})
-
-app.listen(app.get('port'), () => {
-  console.log(`Node app is running on port ${app.get('port')}`)
-})
+app.listen(port(), 
+  lazyLog(`Node app is running on port ${app.get('port')}`))
